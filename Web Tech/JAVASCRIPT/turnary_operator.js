95f2c80bd421b9252b161eae56aaebbfd6eef5fd
Vishnuvardhan M <107617439+vishnu-mekala@users.noreply.github.com>
2022-06-20 01:16:50 +0530

    Delete Web Tech directory

--- Web Tech/JAVASCRIPT/turnary_operator.js
+++
@@ -1,3 +0,0 @@
-document.write("<h2> Turnary Operator</h2>")
-
-10 == 10? document.write("Positive") : document.write("Negative")
--- Web Tech/JAVASCRIPT/unary_operator.js
+++
@@ -1,5 +0,0 @@
-document.write("<h2> Unary Operator</h2>")
-let a = 10
-let b = 5
-let x = a++ + b++ + --a - ++b - a-- + b -- - ++a
-document.write(x)
